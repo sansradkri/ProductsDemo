de1d9efbb34fb89db93a30391db3f9e9faa30e15
Sankalp Malhotra <65014512+sansradkri@users.noreply.github.com>
2020-05-08 16:30:15 +0530

    Version 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+     ProductListComponent, 
+     ConvertToSpacesPipe,
+     StarComponent,
+     ProductDetailComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule, HttpClientModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
